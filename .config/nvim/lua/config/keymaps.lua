cb159a16c6cca78bff736b69c70e7065a1f6518d
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-05 15:20:04 +0600

    fixed the system clipboard copy pasting in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,8 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save File" })
 vim.keymap.set("n", "Q", "<cmd>bd<CR>", { desc = "Close Buffer" })
 
 -- Map <leader>y to yank to system clipboard
--- vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to System Clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to System Clipboard" })
 
 -- Map <leader>p to paste from system clipboard
--- vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from System Clipboard" })
--- vim.keymap.set("v", "<leader>p", '"+p', { desc = "Paste from System Clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from System Clipboard" })
+vim.keymap.set("v", "<leader>p", '"+p', { desc = "Paste from System Clipboard" })
