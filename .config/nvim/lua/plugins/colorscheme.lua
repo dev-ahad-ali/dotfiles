dcef524f0239cbf0f2effa6ab30b8ee688acaa6b
dev-ahad-ali <ahadali.dev@gmail.com>
2026-05-09 01:24:30 +0600

    Update: fixed the colorshceme not loading light variant issue

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,20 +5,18 @@ return {
     priority = 1000,
     config = function()
       require("leaf").setup({
-        {
-          underlineStyle = "underline",
-          commentStyle = "italic",
-          functionStyle = "NONE",
-          keywordStyle = "italic",
-          statementStyle = "bold",
-          typeStyle = "NONE",
-          variablebuiltinStyle = "italic",
-          transparent = false,
-          colors = {},
-          overrides = {},
-          theme = "light", -- default, based on vim.o.background, alternatives: "light", "dark"
-          contrast = "low", -- default, alternatives: "medium", "high"
-        },
+        underlineStyle = "underline",
+        commentStyle = "italic",
+        functionStyle = "NONE",
+        keywordStyle = "italic",
+        statementStyle = "bold",
+        typeStyle = "NONE",
+        variablebuiltinStyle = "italic",
+        transparent = false,
+        colors = {},
+        overrides = {},
+        theme = "light",
+        contrast = "low",
       })
       require("leaf").load()
     end,
