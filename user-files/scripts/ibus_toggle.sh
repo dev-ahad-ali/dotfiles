43220e7a6fb2e6c8f890c8d478b348bd33f8745a
dev-ahad-ali <ahadali.dev@gmail.com>
2026-06-09 16:04:32 +0600

    Update: updated the toggle ibus script

--- user-files/scripts/ibus_toggle.sh
+++ user-files/scripts/ibus_toggle.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-# Toggle KDE Virtual Keyboard between IBus Wayland and None
-# Equivalent to changing System Settings > Input Devices > Virtual Keyboard
-
-CONFIG_FILE="$HOME/.config/kwinrc"
-
 # Read current InputMethod setting
-current_im=$(kreadconfig6 --file kwinrc --group Wayland --key InputMethod 2>/dev/null)
+CURRENT_IM=$(kreadconfig6 --file kwinrc --group Wayland --key InputMethod 2>/dev/null)
+IBUS_PATH="/usr/share/applications/org.freedesktop.IBus.Panel.Wayland.Gtk3.desktop"
 
-if [ "$current_im" = "/usr/share/applications/org.freedesktop.IBus.Panel.Wayland.Gtk3.desktop" ] || [ "$current_im" = "/usr/share/applications/ibus-wayland.desktop" ]; then
-  # Currently IBus, switch to None
-  kwriteconfig6 --file kwinrc --group Wayland --key InputMethod ""
-
-  # Notify KWin about the config change
-  busctl --user emit /kwinrc org.kde.kconfig.notify ConfigChanged "a{saay}" 1 Wayland 1 11 73 110 112 117 116 77 101 116 104 111 100 2>/dev/null
-
-  notify-send "Virtual Keyboard" "Disabled IBus Wayland for Open Bangla Keyboard" -i input-keyboard-virtual-off
+# Toggle the target value
+if [ "$CURRENT_IM" = "$IBUS_PATH" ] || [ "$CURRENT_IM" = "/usr/share/applications/ibus-wayland.desktop" ]; then
+  NEW_IM=""
 else
-  # Currently None or something else, switch to IBus
-  kwriteconfig6 --file kwinrc --group Wayland --key InputMethod "/usr/share/applications/org.freedesktop.IBus.Panel.Wayland.Gtk3.desktop"
-
-  # Notify KWin about the config change
-  busctl --user emit /kwinrc org.kde.kconfig.notify ConfigChanged "a{saay}" 1 Wayland 1 11 73 110 112 117 116 77 101 116 104 111 100 2>/dev/null
-
-  notify-send "Virtual Keyboard" "Enabled IBus Wayland for Open Bangla Keyboard" -i input-keyboard
+  NEW_IM="$IBUS_PATH"
 fi
+
+# Write the new configuration
+kwriteconfig6 --file kwinrc --group Wayland --key InputMethod "$NEW_IM"
+
+# Notify KWin about the config change (only needed once here)
+busctl --user emit /kwinrc org.kde.kconfig.notify ConfigChanged "a{saay}" 1 Wayland 1 11 73 110 112 117 116 77 101 116 104 111 100 2>/dev/null
