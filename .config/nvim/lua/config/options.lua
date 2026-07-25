9a3007594b2db6046dd95a00bde991646747e092
dev-ahad-ali <ahadali.dev@gmail.com>
2026-07-25 15:37:08 +0600

    cofigured neovim to use built in terminal theme colors

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,5 +4,9 @@
 
 vim.opt.clipboard = ""
 vim.opt.wrap = true
+vim.cmd.colorscheme("default") -- Falls back to Neovim's basic layout
+vim.opt.termguicolors = false -- Tells Neovim to respect the terminal's 16/256 ANSI colors
+vim.cmd("set t_Co=256") -- Ensures 256 terminal color compatibility
+
 -- Enable system clipboard integration
 -- vim.opt.clipboard = "unnamedplus"
