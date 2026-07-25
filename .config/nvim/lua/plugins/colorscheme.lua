39b9be63e2badfc3cdb4ed3aa339b204487efd77
dev-ahad-ali <ahadali.dev@gmail.com>
2026-07-25 15:34:08 +0600

    cofigured neovim to use built in terminal theme colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,8 @@
 return {
   {
-    "tiesen243/vercel.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("vercel").setup({
-        theme = "dark",
-        transparent = false, -- Boolean: Sets the background to transparent (Default: false)
-        italics = {
-          comments = true, -- Boolean: Italicizes comments (Default: true)
-          keywords = true, -- Boolean: Italicizes keywords (Default: true)
-          functions = true, -- Boolean: Italicizes functions (Default: true)
-          strings = true, -- Boolean: Italicizes strings (Default: true)
-          variables = true, -- Boolean: Italicizes variables (Default: true)
-          bufferline = false, -- Boolean: Italicizes bufferline (Default: false)
-        },
-        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-      })
-
-      -- This must be called before setting the colorscheme, otherwise it will always default to light mode
-      vim.cmd.colorscheme("vercel")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function() end, -- Prevents LazyVim from forcing a theme
+    },
   },
 }
